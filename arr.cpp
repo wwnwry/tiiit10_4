8b30ea002eeb96ad2317c094ad26c883f2745b19
wwnwry <vladimirovnadara679@gmail.com>
2025-12-12 19:02:47 +0300

    Update arr.cpp

--- arr.cpp
+++ arr.cpp
@@ -4,6 +4,10 @@ using namespace std;
 int main() {
     int arr[] = {3, 7, -2, 10, 0, 5};
     int n = sizeof(arr) / sizeof(arr[0]);
+    if(n==0){
+        cout<<"Массив пуст" <<endl
+            return 1;
+    }
 
     int mn = arr[0];
     int mx = arr[0];
@@ -19,7 +23,7 @@ int main() {
     double avg = static_cast<double>(sum) / n;
 
     cout << "min = " << mn << endl;
-    cout < "max = " << mx << endl;
+    cout << "max = " << mx << endl;
     cout << "avg = " << avg << endl;
 
     return 0;
